549378d6525889662046e378fdd02e21c1a02c24
Romich <hrushch.rr57@gmail.com>
2019-03-21 15:58:39 +0200

    Додав розшифровку малих літер

--- main.js
+++ main.js
@@ -6,43 +6,114 @@ let result = document.getElementById('main_result');
 btn.addEventListener('click', goResult, false);
 
 
-function goResult(params) {
-let crypto = input.value;
-let nresult = [];
-for (var i = 0; i < crypto.length; i++) {
-	switch (crypto.charAt(i)) {
-		case 'h':
-		nresult[i]= 'р'; 
-		  break;
-		  case 'j':
-		  nresult[i]= 'о'; ;
-			break;
-			case 'v':
-			nresult[i]= 'м'; ;
-			  break;
-			  case 'f':
-			  nresult[i]= 'а'; ;
+function goResult() {
+	let crypto = input.value;
+	let nresult = [];
+	for (var i = 0; i < crypto.length; i++) {
+		switch (crypto.charAt(i)) {
+			case 'q':
+				nresult[i] = 'й';
 				break;
-		// 		case 'h':
-		// nresult[i]= 'р'; ;
-		//   break;
-		//   case 'h':
-		// nresult[i]= 'р'; ;
-		//   break;
-		//   case 'h':
-		// nresult[i]= 'р'; ;
-		//   break;
-		default:
-		nresult[i]= '?';
-	  }
-	//   nresult[i]=crypto.charAt(i);
-    // console.log(crypto.charAt(i));
-}
+			case 'w':
+				nresult[i] = 'ц';
+				break;
+			case 'e':
+				nresult[i] = 'у';
+				break;
+			case 'r':
+				nresult[i] = 'к';
+				break;
+			case 't':
+				nresult[i] = 'е';
+				break;
+			case 'y':
+				nresult[i] = 'н';
+				break;
+			case 'u':
+				nresult[i] = 'г';
+				break;
+			case 'i':
+				nresult[i] = 'ш';
+				break;
+			case 'o':
+				nresult[i] = 'щ';
+				break;
+			case 'p':
+				nresult[i] = 'з';
+				break;
+			case '[':
+				nresult[i] = 'х';
+				break;
+			case ']':
+				nresult[i] = 'ї';
+				break;
+			case 'a':
+				nresult[i] = 'ф';
+				break;
+			case 's':
+				nresult[i] = 'і';
+				break;
+			case 'd':
+				nresult[i] = 'в';
+				break;
+			case 'f':
+				nresult[i] = 'а';
+				break;
+			case 'g':
+				nresult[i] = 'п';
+				break;
+			case 'h':
+				nresult[i] = 'р';
+				break;
+			case 'j':
+				nresult[i] = 'о';
+				break;
+			case 'k':
+				nresult[i] = 'л';
+				break;
+			case 'l':
+				nresult[i] = 'д';
+				break;
+			case ';':
+				nresult[i] = 'ж';
+				break;
+			case 'z':
+				nresult[i] = 'я';
+				break;
+			case 'x':
+				nresult[i] = 'ч';
+				break;
+			case 'c':
+				nresult[i] = 'с';
+				break;
+			case 'v':
+				nresult[i] = 'м';
+				break;
+			case 'b':
+				nresult[i] = 'и';
+				break;
+			case 'n':
+				nresult[i] = 'т';
+				break;
+			case 'm':
+				nresult[i] = 'ь';
+				break;
+			case ',':
+				nresult[i] = 'б';
+				break;
+			case '.':
+				nresult[i] = 'ю';
+				break;
+			default:
+				nresult[i] = '?';
+		}
 
-console.log(nresult);
+	}
+
+	console.log(nresult);
 
 
-	 result.textContent = nresult.join('');
+	result.textContent = nresult.join('');
 	// console.log(input.value);
-	
+
 }
