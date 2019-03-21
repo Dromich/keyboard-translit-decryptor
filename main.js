d5ee6d03625d6bca110da0f589e143c6b965d9fb
Romich <hrushch.rr57@gmail.com>
2019-03-21 17:15:52 +0200

    Додав великі літери

--- main.js
+++ main.js
@@ -104,6 +104,99 @@ function goResult() {
 			case '.':
 				nresult[i] = 'ю';
 				break;
+				case 'Q':
+				nresult[i] = 'Й';
+				break;
+			case 'W':
+				nresult[i] = 'Ц';
+				break;
+			case 'E':
+				nresult[i] = 'У';
+				break;
+			case 'R':
+				nresult[i] = 'К';
+				break;
+			case 'T':
+				nresult[i] = 'Е';
+				break;
+			case 'Y':
+				nresult[i] = 'Н';
+				break;
+			case 'U':
+				nresult[i] = 'Г';
+				break;
+			case 'I':
+				nresult[i] = 'Ш';
+				break;
+			case 'O':
+				nresult[i] = 'Щ';
+				break;
+			case 'P':
+				nresult[i] = 'З';
+				break;
+			case '{':
+				nresult[i] = 'Х';
+				break;
+			case '}':
+				nresult[i] = 'Ї';
+				break;
+			case 'A':
+				nresult[i] = 'Ф';
+				break;
+			case 'S':
+				nresult[i] = 'І';
+				break;
+			case 'D':
+				nresult[i] = 'В';
+				break;
+			case 'F':
+				nresult[i] = 'А';
+				break;
+			case 'G':
+				nresult[i] = 'П';
+				break;
+			case 'H':
+				nresult[i] = 'Р';
+				break;
+			case 'J':
+				nresult[i] = 'О';
+				break;
+			case 'K':
+				nresult[i] = 'Л';
+				break;
+			case 'L':
+				nresult[i] = 'Д';
+				break;
+			case ':':
+				nresult[i] = 'Ж';
+				break;
+			case 'Z':
+				nresult[i] = 'Я';
+				break;
+			case 'X':
+				nresult[i] = 'Ч';
+				break;
+			case 'C':
+				nresult[i] = 'С';
+				break;
+			case 'V':
+				nresult[i] = 'М';
+				break;
+			case 'B':
+				nresult[i] = 'И';
+				break;
+			case 'N':
+				nresult[i] = 'Т';
+				break;
+			case 'M':
+				nresult[i] = 'Ь';
+				break;
+			case '<':
+				nresult[i] = 'Б';
+				break;
+			case '>':
+				nresult[i] = 'Ю';
+				break;
 			default:
 				nresult[i] = '?';
 		}
