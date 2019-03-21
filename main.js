edc2257823f9b645b06732abe17874f089adf828
Romich <hrushch.rr57@gmail.com>
2019-03-21 15:48:24 +0200

    Зробив базову логіку

--- main.js
+++ main.js
@@ -7,7 +7,42 @@ btn.addEventListener('click', goResult, false);
 
 
 function goResult(params) {
-	result.textContent = input.value;
-	console.log(input.value);
+let crypto = input.value;
+let nresult = [];
+for (var i = 0; i < crypto.length; i++) {
+	switch (crypto.charAt(i)) {
+		case 'h':
+		nresult[i]= 'р'; 
+		  break;
+		  case 'j':
+		  nresult[i]= 'о'; ;
+			break;
+			case 'v':
+			nresult[i]= 'м'; ;
+			  break;
+			  case 'f':
+			  nresult[i]= 'а'; ;
+				break;
+		// 		case 'h':
+		// nresult[i]= 'р'; ;
+		//   break;
+		//   case 'h':
+		// nresult[i]= 'р'; ;
+		//   break;
+		//   case 'h':
+		// nresult[i]= 'р'; ;
+		//   break;
+		default:
+		nresult[i]= '?';
+	  }
+	//   nresult[i]=crypto.charAt(i);
+    // console.log(crypto.charAt(i));
+}
+
+console.log(nresult);
+
+
+	 result.textContent = nresult.join('');
+	// console.log(input.value);
 	
 }
